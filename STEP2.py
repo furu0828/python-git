f0cc738faeef163f36c9c9af9dc90dd7bd04c99f
masaru <furukawamasaru08@gmail.com>
2026-03-20 06:46:37 +0900

    UPDATE1

--- STEP2.py
+++ STEP2.py
@@ -4,3 +4,9 @@ data = np.array([10, 20, 30, 40])
 print(np.mean(data))
 print(np.var(data))
 print(np.std(data))
+
+print(np.max(data))
+print(np.min(data))
+
+data2 = data * 2
+print(np.mean(data2))
