f9f1db23d149412d8346f4a4989d2480da228e35
masaru <furukawamasaru08@gmail.com>
2026-03-20 06:57:51 +0900

    initial

---
+++ STEP3.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.DataFrame({
+    "age": [20, 30, 40],
+    "income": [300, 500, 700]
+})
+
+# print(df)
+# print(df.describe())
+
+df["score"] = [70, 80, 90]
+
+print(df["age"].mean())
