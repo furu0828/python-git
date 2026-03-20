a05a3e0b9ba4993c8bae757f207b2f06f785a714
masaru <furukawamasaru08@gmail.com>
2026-03-20 17:14:21 +0900

    UPDATE2

--- STEP4.py
+++ STEP4.py
@@ -2,9 +2,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.DataFrame({
-    "age": [20, 25, 30, 35, 40, 45, 50]
+    "age": [20, 25, 30, 35, 40],
+    "income": [300, 350, 500, 650, 800]
 })
 
-
-plt.hist(df["age"], bins=5)
+plt.scatter(df["age"], df["income"])
 plt.show()
